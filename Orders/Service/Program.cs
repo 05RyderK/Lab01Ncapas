23fcce7b966da9c8ee1befb1d37b75b109a05fa3
Andres Duarte <andres_fduarte@soy.sena.edu.co>
2024-07-30 23:19:31 -0500

    feat(back-end) Add layer Services

---
+++ Orders/Service/Program.cs
@@ -0,0 +1,38 @@
+using BLL;
+using Entities.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddTransient<Customers>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(name: "default", pattern: "api/{controller}/{action}/{id?}");
+});
+
+app.Run();

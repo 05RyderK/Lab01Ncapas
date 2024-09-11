e67faf5972a8698c89aa6d512ba0927aefae35ec
Andres Duarte <andres_fduarte@soy.sena.edu.co>
2024-09-11 17:06:02 -0500

    feat(back-end): Add Interface service a Product

--- Orders/Entities/Models/Product.cs
+++ Orders/Entities/Models/Product.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Entities.Models;
 
 [Table("Product")]
-//[Index("ProductName", Name = "IndexProductName")]
-//[Index("SupplierId", Name = "IndexProductSupplierId")]
+[Index("ProductName", Name = "IndexProductName")]
+[Index("SupplierId", Name = "IndexProductSupplierId")]
 public partial class Product
 {
     [Key]
@@ -20,17 +20,22 @@ public partial class Product
     public int SupplierId { get; set; }
 
     [Column(TypeName = "decimal(12, 2)")]
-    public decimal? UnitPrice { get; set; }
+    public double? UnitPrice { get; set; }
 
     [StringLength(30)]
     public string? Package { get; set; }
 
-    public bool IsDiscontinued { get; set; }
+    public bool? IsDiscontinued { get; set; }
 
     [InverseProperty("Product")]
-    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    public virtual ICollection<OrderItem>? OrderItems { get; set; } = new List<OrderItem>();
 
     [ForeignKey("SupplierId")]
     [InverseProperty("Products")]
-    public virtual Supplier Supplier { get; set; } = null!;
+    public virtual Supplier? Supplier { get; set; } = null!;
+
+    public Product()
+    {
+        this.IsDiscontinued = false;
+    }
 }
